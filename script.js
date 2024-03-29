1a31d67ac6d5f53c3db0cb9653b34a8a65542e52
Whitneyas <whitneyserwaa@gmail.com>
2024-03-29 16:27:52 +0100

    added camera effect

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const activateCameraButton = document.getElementById('activate-camera');
+const videoElement = document.getElementById('video-element');
+
+// Event listener for the button click
+activateCameraButton.addEventListener('click', async () => {
+  try {
+    // Request access to user's camera
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+    // Display the camera stream in the video element
+    videoElement.srcObject = stream;
+  } catch (error) {
+    console.error('Error accessing camera:', error);
+  }
+});
