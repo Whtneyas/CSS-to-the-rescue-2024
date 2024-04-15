b959377a9ab837dcf88e2293ce1f044f01f704c2
Whitneyas <whitneyserwaa@gmail.com>
2024-04-15 18:43:41 +0200

    Gallery added

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-const activateCameraButton = document.getElementById('activate-camera');
-const videoElement = document.getElementById('video-element');
+// const activateCameraButton = document.getElementById('activate-camera');
+// const videoElement = document.getElementById('video-element');
 
-// Event listener for the button click
-activateCameraButton.addEventListener('click', async () => {
-  try {
-    // Request access to user's camera
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+// // Event listener for the button click
+// activateCameraButton.addEventListener('click', async () => {
+//   try {
+//     // Request access to user's camera
+//     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-    // Display the camera stream in the video element
-    videoElement.srcObject = stream;
-  } catch (error) {
-    console.error('Error accessing camera:', error);
-  }
-});
+//     // Display the camera stream in the video element
+//     videoElement.srcObject = stream;
+//   } catch (error) {
+//     console.error('Error accessing camera:', error);
+//   }
+// });
